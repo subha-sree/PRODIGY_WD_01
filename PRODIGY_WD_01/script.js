8e21b7731aa5602526e3a24edf2b2856cef23934
SUBHA SREE MURUGAN <165561024+subha-sree@users.noreply.github.com>
2024-12-24 21:47:16 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,6 @@
+// If you want to implement the hamburger menu on mobile devices
+function toggleMenu() {
+    const navIcons = document.querySelector('.nav-icons');
+    navIcons.classList.toggle('active');
+  }
+  
